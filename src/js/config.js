f7fd8d8ce4c5e45187e1366ace03e944f9fee70c
gvssaimadhav <saimadhavgovindu@gmail.com>
2022-04-23 22:17:02 +0530

    V1.0

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '96e44fd3-b77e-4f86-8f14-d7f60c4af7d2';
+export const MODAL_CLOSE_SEC = 2.5;
